b54ab124a6066ca7c33a0d5724b48f9e9eef3072
wangqiaowen <39010198+wangqiaowen@users.noreply.github.com>
2021-05-02 23:19:45 +0200

    Update admin.py

--- user_info/admin.py
+++ user_info/admin.py
@@ -5,11 +5,8 @@ from sector.models import Sector
 # Register your models here.
 
 class UserInfoAdmin(admin.ModelAdmin):
-    # form = BankcardsForm
-    # search_fields = ['username']
     fields = ('userid','userfirstname', 'userlastname', 'useremail', 'userscale', 'sectorid', 'organizationid')
     list_display = ('userid','userfirstname', 'userlastname', 'modifytime')
-    # list_filter = ('sector','is_valid',)
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "sectorid":
