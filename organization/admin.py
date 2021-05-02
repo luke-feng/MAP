66679db8e397ba02999d3f9f6bfdebe3aa0e0321
wangqiaowen <39010198+wangqiaowen@users.noreply.github.com>
2021-05-02 23:16:19 +0200

    Update admin.py

--- organization/admin.py
+++ organization/admin.py
@@ -13,70 +13,7 @@ import datetime
 
 # Register your models here.
 
-# class OrganizationAdmin(admin.ModelAdmin):
-#     change_form_template = 'record_change_form.html'
-#     # list_display = ('organization_name', 'modify_time')
-#     # fields = ('organization_name','country','city','revenue',)
-#
-#     def change_view(self, request, object_id, form_url='', extra_context=None):
-#         extra_context = extra_context or {}
-#         extra_context['osm_data'] = self.get_dynamic_info()
-#         return super(OrganizationAdmin, self).change_view(
-#             request, object_id, form_url, extra_context=extra_context,
-#         )
-
-# class DateForm(forms.ModelForm):
-#     from_date = forms.DateField(widget=AdminDateWidget())
-#
-# class CustomerAdmin(admin.ModelAdmin):
-#     formfield_overrides = {
-#         models.DateField: { 'widget': AdminDateWidget },
-#     }
-#
-# class DateSelectorWidget(forms.MultiWidget):
-#     def __init__(self, attrs=None):
-#         year_now = datetime.datetime.now().year
-#         # print (year_now)
-#         days = [(day, day) for day in range(1, 32)]
-#         months = [(month, month) for month in range(1, 13)]
-#         years = [(year, year) for year in range(1700, year_now+1)]
-#         widgets = [
-#             forms.Select(attrs=attrs, choices=days),
-#             forms.Select(attrs=attrs, choices=months),
-#             forms.Select(attrs=attrs, choices=years),
-#         ]
-#         super().__init__(widgets, attrs)
-#
-#     def decompress(self, value):
-#         if isinstance(value, date):
-#             return [value.day, value.month, value.year]
-#         elif isinstance(value, str):
-#             year, month, day = value.split('-')
-#             return [day, month, year]
-#         return [None, None, None]
-#
-#     def value_from_datadict(self, data, files, name):
-#         day, month, year = super().value_from_datadict(data, files, name)
-#         # DateField expects a single string that it can parse into a date.
-#         return '{}-{}-{}'.format(year, month, day)
-#
-# def validate_date(date):
-#     if date > timezone.now().date():
-#         raise ValidationError("Date cannot be in the past")
-#
-# class BankcardsForm(forms.ModelForm):
-#     # date_of_establishment = forms.DateField(widget=[forms.TextInput(attrs={'placeholder': u'DD-MM-YYY'}, forms.SelectDateWidget(empty_label="Nothing") ] ))
-#     date_of_establishment = forms.DateField(widget=DateSelectorWidget, validators=[validate_date,])
-
-
-    # code = forms.CharField(validators=[bankcode_validate, ], widget=forms.TextInput(attrs={'placeholder': u'输入银行编号'}))
-
-
-
-
-
 class OrganizationAdmin(admin.ModelAdmin):
-    # form = BankcardsForm
     search_fields = ['organization_name']
     fields = ('organization_name', 'date_of_establishment', 'country', 'city', 'revenue', 'it_employee_scale', 'employee_scale', 'sector','is_valid')
     list_display = ('organization_name', 'sector', 'modify_time','is_valid')
@@ -88,6 +25,3 @@ class OrganizationAdmin(admin.ModelAdmin):
         return super(OrganizationAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
 admin.site.register(Organization, OrganizationAdmin)
-# admin.site.register(BankcardsForm)
-# admin.site.register(City)
-# admin.site.register(Country)
