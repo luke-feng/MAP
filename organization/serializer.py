be61e59a1516fac0ec12073155f2e5cc5adf13db
wangqiaowen <39010198+wangqiaowen@users.noreply.github.com>
2021-05-02 23:18:02 +0200

    Update serializer.py

--- organization/serializer.py
+++ organization/serializer.py
@@ -4,10 +4,4 @@ from .models import Organization
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
-        # fields = [
-        #         #     'sectorid',
-        #         #     'sector_name',
-        #         #     'modify_time',
-        #         #     'is_valid',
-        #         # ]
         fields = '__all__'
